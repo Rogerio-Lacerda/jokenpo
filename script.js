ea5ad9846ab23a2a2246133d03ed56137a094a8a
Rogerio-Lacerda <70972583+Rogerio-Lacerda@users.noreply.github.com>
2023-05-24 17:41:50 -0300

    Crie uma função que é acionada ao clique do usuário, ele pega o valor do dataset do botao que o usuario clicou, e depois ela pega um valor sortead de dataset de todos os botões.

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ function tamanhoImgs() {
 tamanhoImgs();
 
 window.addEventListener("resize", tamanhoImgs);
+
+let escolhasSorteio = [];
+function getEscolhas(event) {
+  const escolhaJogador = event.currentTarget.dataset.escolhas;
+  const escolhasArray = [...escolhas];
+  escolhasArray.map(() => {
+    const numero = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+    escolhasSorteio.push(escolhasArray[numero]);
+  });
+  const escolhaCpu = escolhasSorteio[0].dataset.escolhas;
+  escolhasSorteio = [];
+}
+
+escolhas.forEach((item) => {
+  item.addEventListener("click", getEscolhas);
+});
